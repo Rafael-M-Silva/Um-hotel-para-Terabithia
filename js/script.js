094f2d54b634843863e2886f35de76990439819b
Rafael Mauricio <rafael_mauricio@hotmail.com>
2023-10-31 09:19:07 -0300

    funcao menu switch case ok

--- js/script.js
+++ js/script.js
@@ -5,11 +5,56 @@ const nomeUsuario = window.prompt("Qual o nome de usuario?");
 const senhaUsuario = window.prompt("Qual a senha do usuario?");
 const senha = 2678;
 
+const menu = () => {
+  let opcao = window.prompt(`
+    1: para chamar a func 1.
+    2: para chamar a func 2.
+    3: para chamar a func 3.
+    4: para chamar a func 4.
+    5: para chamar a func 5.
+    6: para chamar a func 6.
+    7: para chamar a func 7.
+    0: para sair do sistema!.
+  `);
+
+  switch (opcao) {
+    case "1":
+      reservaQuarto();
+      break;
+    case "2":
+      cadastroHospede();
+      break;
+    case "3":
+      pesquisaHospede();
+      break;
+    case "4":
+      cadastroEmpresaEvento();
+      break;
+    case "5":
+      buffetHotel();
+      break;
+    case "6":
+      abastecerCarro();
+      break;
+    case "7":
+      manutencaoArCondicionado();
+      break;
+    case "0":
+      sair();
+      break;
+    default:
+      alert("Digito invalido! Vamos tentar novamente :)");
+      menu();
+      break;
+  }
+};
+
 const inicio = () => {
   alert(
     `Bem vindo ao Hotel ${nomeHotel}, ${nomeUsuario}. É um imenso prazer ter você por aqui!".`
   );
-  reservaQuarto();
+
+  menu();
 };
 
 const sair = () => {
@@ -17,15 +62,44 @@ const sair = () => {
 };
 
 const reservaQuarto = () => {
-  const valorDiaria = Number(window.prompt("Qual o valor padrão da diária?"));
+  alert("funcao reservar quarto");
+  inicio();
+};
 
-  if (valorDiaria <= 0) {
-    alert(`Valor inválido, ${nomeUsuario}`);
-    inicio();
-    return;
-  }
+const cadastroHospede = () => {
+  alert("funcao cadastroHospede");
+  inicio();
+};
 
-  alert("proxima funcao");
+const pesquisaHospede = () => {
+  alert("funcao pesquisaHospede");
+  inicio();
+};
+
+/* Eventos do hotel */
+
+const cadastroEmpresaEvento = () => {
+  alert("funcao cadastroEmpresaEvento");
+  inicio();
+};
+
+const buffetHotel = () => {
+  alert("funcao buffetHotel");
+  inicio();
+};
+
+const auditorioEvento = () => {};
+
+const dataEvento = () => {};
+
+const abastecerCarro = () => {
+  alert("funcao abastecerCarro");
+  inicio();
+};
+
+const manutencaoArCondicionado = () => {
+  alert("funcao manutencaoArCondicionado");
+  inicio();
 };
 
 if (senhaUsuario == senha) {
